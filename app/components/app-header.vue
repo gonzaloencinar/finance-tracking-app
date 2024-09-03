67e61c517d0067650c4d4125871446d7821a7ee7
Gonzalo Encinar <gonzalo.encinar.contractor@bbva.com>
2024-09-03 19:44:34 +0200

    boilerplate nuxt code

---
+++ app/components/app-header.vue
@@ -0,0 +1,11 @@
+import { NuxtLink } from '../../.nuxt/components';
+<template>
+  <header class="flex justify-between items-center"></header>
+  <NuxtLink to="/" class="text-xl font-bold">Finace Tracker</NuxtLink>
+  <div>
+    <UAvatar
+      src="https://avatars.githubusercontent.com/u/739984?v=4"
+      alt="Avatar"
+    />
+  </div>
+</template>
