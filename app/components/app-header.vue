43291a927537133e6b535818ba3c5688c561de44
Gonzalo Encinar <gonzalo.encinar.contractor@bbva.com>
2024-09-03 19:47:24 +0200

    flex distribution

--- app/components/app-header.vue
+++ app/components/app-header.vue
@@ -1,11 +1,11 @@
-import { NuxtLink } from '../../.nuxt/components';
 <template>
-  <header class="flex justify-between items-center"></header>
-  <NuxtLink to="/" class="text-xl font-bold">Finace Tracker</NuxtLink>
-  <div>
-    <UAvatar
-      src="https://avatars.githubusercontent.com/u/739984?v=4"
-      alt="Avatar"
-    />
-  </div>
+  <header class="flex justify-between items-center">
+    <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </header>
 </template>
