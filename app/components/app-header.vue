f89fb4979ce4536f96e70fa257c503f7e80439f2
Gonzalo Encinar <gonzalo.encinar.contractor@bbva.com>
2024-09-03 20:42:12 +0200

    until lesson 60

--- app/components/app-header.vue
+++ app/components/app-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between items-center">
+  <header class="flex justify-between items-center mt-10">
     <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
     <div>
       <UAvatar
